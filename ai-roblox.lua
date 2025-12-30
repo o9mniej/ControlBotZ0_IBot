61c0c97cd75b6d8ecbda254f52391194bf15ae2b
o9mniej <123626474+o9mniej@users.noreply.github.com>
2025-12-30 09:39:10 +0100

    Update ai-roblox.lua

--- ai-roblox.lua
+++ ai-roblox.lua
@@ -1,11 +1,13 @@
 local botz = loadstring(game:HttpGet("https://raw.githubusercontent.com/o9mniej/ControlBotZ0_IBot/refs/heads/main/ControlBotZ%20Module.lua"))()
 botz.Prefix = "."
-botz.Bots = {"IBot1"}
+botz.Bots = {"IBot"}
 botz:addAdmin("o9mniej1")
 botz:addAdmin("TheRealThomasPlayz")
 botz:addAdmin("N93333")
+botz:Chat("test")
 function mainFunction(player, message)
   if message == botz.Prefix .. "test" then -- So you need to type .test instead of just test
     botz:Chat("Hello World")
   end
+
 end
